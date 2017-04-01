caf8d941ea11da78a0780027f3f608dc5fffd9c9
dabuddahn <mark.sukhram@upr.edu>
2017-04-01 17:31:04 -0400

    Spark Solution 1

--- spark/p3a.py
+++ spark/p3a.py
@@ -1,1 +1,7 @@
 #Code here
+import csv
+school_data = sc.textFile("/home/dabuddahn/exam1-spring-17-big-data-dabuddahn/hive/escuelasPR.csv")
+student_data = sc.textFile("/home/dabuddahn/exam1-spring-17-big-data-dabuddahn/hive/studentsPR.csv")
+student_list=student_data.map(lambda x : [x]).map(lambda x : list(csv.reader(x))[0])
+result=student_list.filter(lambda x : x[5] == 'F').filter(lambda x : x[2] == '71381')
+result.foreach(print)
