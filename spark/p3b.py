c2c0893a48c68e34b4ec99bcc1b857533e28c491
dabuddahn <mark.sukhram@upr.edu>
2017-04-01 17:38:01 -0400

    Spark Solution 2

--- spark/p3b.py
+++ spark/p3b.py
@@ -1,1 +1,9 @@
 #Code here
+import csv
+school_data = sc.textFile("/home/dabuddahn/exam1-spring-17-big-data-dabuddahn/hive/escuelasPR.csv")
+school_list=school_data.map(lambda x: [x]).map(lambda x : list(csv.reader(x))[0])
+ponce_city = school_list.filter(lambda x: x[2] == 'Ponce')
+cabo_rojo_city = school_list.filter(lambda x: x[2] == 'Cabo Rojo')
+dorado_city = school_list.filter(lambda x: x[2] == 'Dorado')
+result1 = ponce_city.union(cabo_rojo_city).union(dorado_city)
+result2.foreach(print)
